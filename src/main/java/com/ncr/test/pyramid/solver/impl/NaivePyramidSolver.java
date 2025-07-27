48277d3e52942c76c7a16de1d7564e61d35229fa
TomaszPierzchala <t.pierzchala@gmail.com>
2025-07-27 23:51:24 +0200

    task 2 - impróvment + comments of NaivePyramidSolver

--- src/main/java/com/ncr/test/pyramid/solver/impl/NaivePyramidSolver.java
+++ src/main/java/com/ncr/test/pyramid/solver/impl/NaivePyramidSolver.java
@@ -13,11 +13,16 @@ public class NaivePyramidSolver implements PyramidSolver {
     }
 
     private long getTotalAbove(int row, int column, Pyramid pyramid) {
-        if (row == 0) return 0;
+        if (row == 0) return pyramid.get(row, column); // it should take values from highest row, not Zeros; 
 
         int myValue = pyramid.get(row, column);
         long left  = myValue + getTotalAbove(row - 1, column, pyramid);
         long right = myValue + getTotalAbove(row - 1, column + 1, pyramid);
+        /* here are two problems: 
+        1. one row decition is not enough, we need to consider all paths above 
+        in my code I take up to MAX_SIZE Paths (limit the number of paths to avoid OutOfMemory issues)
+        2. eficiency: I do not use recursion of method call, but rather calculating best paths (called SubPath) every row.
+        */
         return Math.max(left, right);
     }
 }
